89b2d3152f8a910766cb48447225e17121dee93d
Devanshi Bilthare <devanshibilthare@gmail.com>
2024-02-10 15:28:17 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,122 @@
+function startAnimation(){
+    var tl = gsap.timeline()
+
+tl.to(".st_img1",{
+    display:"block",
+    duration:.0001
+})
+
+tl.to(".st_img1",{
+    display:"none"
+})
+tl.to(".st_img2",{
+    display:"block",
+    duration:.0001
+})
+
+tl.to(".st_img2",{
+    display:"none"
+})
+tl.to(".st_img3",{
+    display:"block",
+    duration:.0001
+})
+
+tl.to(".st_img3",{
+    display:"none"
+})
+tl.to(".st_img4",{
+    display:"block",
+    duration:.0001
+})
+
+tl.to(".st_img4",{
+    display:"none"
+})
+tl.to(".st_img5",{
+    display:"block",
+    duration:.0001
+})
+
+tl.to(".st_img5",{
+    display:"none"
+})
+tl.to(".st_img6",{
+    display:"block",
+    duration:.0001
+})
+
+tl.to(".st_img6",{
+    display:"none"
+})
+
+tl.to(".st_img1",{
+    display:"block",
+    duration:.0001
+})
+
+tl.to(".st_img1",{
+    display:"none"
+})
+tl.to(".st_img2",{
+    display:"block",
+    duration:.0001
+})
+
+tl.to(".st_img2",{
+    display:"none"
+})
+tl.to(".st_img3",{
+    display:"block",
+    duration:.0001
+})
+
+tl.to("#page1",{
+    transform:"translateY(0)"
+})
+
+tl.to("nav",{
+    zIndex:999,
+})
+
+tl.from('#page1 h1',{
+    transform:"translateY(80%)",
+    stagger:.1,
+    duration:1
+})
+
+tl.to("body",{
+    overflow:"auto"
+})
+}
+startAnimation();
+
+function videoAnimation(){
+    gsap.from("#page2 video",{
+        transform: "scaleX(.3)",
+        scrollTrigger:{
+            trigger:"#page2 video",
+            scroller:"body",
+            // markers:true,
+            end:"top 1%",
+            scrub:true
+        }
+    })
+}
+videoAnimation()
+
+function page3Animation(){
+    gsap.from('#page3 h2',{
+        transform:"translateY(50%)",
+        opacity:0,
+        stagger:.5,
+        duration:1,
+        scrollTrigger:{
+            trigger:"#page3 h2",
+            scroller:"body",
+            // markers:true,
+            start:"top 90%"
+        }
+    })
+}
+page3Animation()
